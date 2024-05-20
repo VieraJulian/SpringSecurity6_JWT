e3a77840152d8b6330668ecf622ebea14805dfef
VieraJulian <julianviera2019@hotmail.com>
2024-05-20 20:32:09 -0300

    Método para crear y validar un token

--- src/main/java/com/app/app/util/JwtUtils.java
+++ src/main/java/com/app/app/util/JwtUtils.java
@@ -1,7 +1,10 @@
 package com.app.app.util;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -24,13 +27,14 @@ public class JwtUtils {
         Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
 
         String username = authentication.getPrincipal().toString();
+
         String authorities = authentication.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
 
-        String jwtToken = JWT.create()
-                .withIssuer(this.userGenerator)
+        return JWT.create()
+                .withIssuer(userGenerator)
                 .withSubject(username)
                 .withClaim("authorities", authorities)
                 .withIssuedAt(new Date())
@@ -38,7 +42,19 @@ public class JwtUtils {
                 .withJWTId(UUID.randomUUID().toString())
                 .withNotBefore(new Date(System.currentTimeMillis()))
                 .sign(algorithm);
-        
-        return jwtToken;
+    }
+
+    public DecodedJWT validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            return verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
     }
 }
